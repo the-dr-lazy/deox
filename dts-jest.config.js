0b5e61b01e6aac88516adb6b409951307eae9176
Mohammad Hasani <thebrodmann@gmail.com>
2019-01-17 15:34:28 +0330

    chore: add dts-jest testing config and scripts

---
+++ dts-jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const { compilerOptions } = require('./tsconfig.json')
+
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: [
+    '**/__tests__/**/*.dts.ts?(x)',
+    '**/?(*.)dts.+(spec|test).ts?(x)',
+  ],
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  transform: {
+    '(/__tests__/.*|(\\.|/)dts\\.(test|spec))\\.tsx?$': 'dts-jest/transform',
+  },
+  globals: {
+    _dts_jest_: {
+      transpile: false,
+      test_value: false,
+      enclosing_declaration: true,
+      compiler_options: 'tsconfig.spec.json',
+    },
+  },
+}
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
   preset: 'ts-jest',
+  testPathIgnorePatterns: [
+    '(/__tests__/.*\\.dts|\\.dts\\.(test|spec))\\.tsx?$',
+  ],
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json',
+    },
+  },
 }
