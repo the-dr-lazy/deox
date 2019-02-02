a16eb49683cec58f84e02900aa9873fe2dedbe96
Mohammad Hasani <thebrodmann@gmail.com>
2019-02-02 16:14:53 +0330

    docs(examples/tasks): show snackbar on category item click

--- examples/tasks/src/app/components/category-item.tsx
+++ examples/tasks/src/app/components/category-item.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import classNames from 'classnames'
+import { createSnackbar } from '~/store'
 
 import { CategoryIndicator } from './category-indicator'
 
-export type CategoryItemProps = {
+export type CategoryItemInnerDispatchProps = {
+  createSnackbar: typeof createSnackbar
+}
+export type CategoryItemInnerOwnProps = {
   color: string
   label: string
   value: string
   active?: boolean
-} & JSX.IntrinsicElements['li']
+  className?: string
+}
+export type CategoryItemInnerProps = CategoryItemInnerDispatchProps &
+  CategoryItemInnerOwnProps
 
-export const CategoryItem = ({
+export const CategoryItemInner = ({
   color,
   label,
   value,
   active,
   className,
-  ...rest
-}: CategoryItemProps) => (
-  <li className={classNames('CategoryItem', className)} {...rest}>
+  createSnackbar,
+}: CategoryItemInnerProps) => (
+  <li
+    className={classNames('CategoryItem', className)}
+    onClick={() =>
+      createSnackbar({
+        title: 'Categories',
+        message: 'This feature has not implemented yet.',
+      })
+    }
+  >
     <CategoryIndicator
       className="CategoryItem__indicator"
       color={color}
@@ -30,3 +46,12 @@ export const CategoryItem = ({
     </div>
   </li>
 )
+
+export const CategoryItem = connect<
+  null,
+  CategoryItemInnerDispatchProps,
+  CategoryItemInnerOwnProps
+>(
+  null,
+  { createSnackbar }
+)(CategoryItemInner)
