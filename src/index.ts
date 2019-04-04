975591c531f28a97f3c87f8cc71161488e383312
Mohammad Hasani <thebrodmann@gmail.com>
2019-04-04 16:15:57 +0430

    refactor: make createAction as an alias for createActionCreator
    closes #30

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export { configureStore } from 'redux-starter-kit'
 
 export { action, Action, AnyAction } from './action'
-export { createActionCreator, ActionCreator } from './create-action-creator'
+export {
+  createActionCreator,
+  createActionCreator as createAction,
+  ActionCreator,
+} from './create-action-creator'
 export { getType } from './get-type'
 export { createReducer } from './create-reducer'
 export { ofType } from './of-type'
