da061bf15066e90bbfe2a1094b644db8a93d0163
Louiz <luizfelipeco97@gmail.com>
2019-08-31 20:47:16 -0300

    feat: remade the overall API to make it type safe

--- src/create-action-creator-factory.ts
+++ src/create-action-creator-factory.ts
@@ -1,56 +1,77 @@
-import { Action } from './create-action'
-import { createActionCreator } from './create-action-creator'
+import { action, Action } from "deox";
 
-type ReplaceActionType<
-  TAction extends Action<string>,
-  TNewType extends string
-> = TAction extends Action<string, infer P, infer M>
-  ? Action<TNewType, P, M>
-  : (TAction extends Action<string, infer P>
-    ? Action<TNewType, P>
-    : (TAction extends Action<string, undefined, infer M>
-      ? Action<TNewType, undefined, M>
-      : Action<TNewType>));
+export type Resolver<TType extends string> = <
+  TPayload = undefined,
+  TMeta = undefined
+  >(
+  payload?: TPayload,
+  meta?: TMeta
+) => Action<TType, TPayload, TMeta>;
 
-type Executor = (
-  resolve: <TPayload = undefined, TMeta = undefined>(
-    payload?: TPayload,
-    meta?: TMeta
-  ) => Action<string, TPayload, TMeta>
-) => (...args: any) => Action<string>;
+type ActionProducer<TType extends string> = (...args: any) => Action<TType>;
 
-type CreatorMap = Record<string, Executor>;
-type ActionTypeMap<T> = Record<keyof T, string>;
+type FactoryActionCreator<
+  TType extends string,
+  TProducer extends ActionProducer<TType>
+  > = TProducer & { type: TType; toString(): TType };
+
+type Executor<TType extends string, TProducer extends ActionProducer<TType>> = (
+  resolve: Resolver<TType>
+) => TProducer;
+
+type ActionCreatorProducer<
+  TType extends string,
+  TProducer extends ActionProducer<TType>
+  > = (type: TType) => FactoryActionCreator<TType, TProducer>;
+
+export type ExecutorHandler = <
+  TType extends string,
+  TProducer extends ActionProducer<TType>
+  >(
+  executor: Executor<TType, TProducer>
+) => ActionCreatorProducer<TType, TProducer>;
+
+type CreatorMap = Record<string, any>;
+
+type ActionProducerMap<T extends CreatorMap> = {
+  [key in keyof T]: (creator: T[key]) => FactoryActionCreator<string, any>;
+};
+
+type ResolverCreator = <TType extends string>(type: TType) => Resolver<TType>;
 
 type ActionCreatorMap<
   TCreators extends CreatorMap,
-  TActionTypes extends ActionTypeMap<TCreators>
-> = {
-  readonly [TKey in keyof TCreators]: {
-    (...args: Parameters<ReturnType<TCreators[TKey]>>): ReplaceActionType<
-      ReturnType<ReturnType<TCreators[TKey]>>,
-      TActionTypes[TKey]
-    >;
-    type: TActionTypes[TKey];
-    toString(): TActionTypes[TKey];
-  };
+  TActionTypes extends ActionProducerMap<TCreators>
+  > = {
+  readonly [TKey in keyof TCreators]: ReturnType<TActionTypes[TKey]>;
 };
 
 type ActionCreatorFactory<TCreators extends CreatorMap> = <
-  TActionTypes extends ActionTypeMap<TCreators>
->(
+  TActionTypes extends ActionProducerMap<TCreators>
+  >(
   types: TActionTypes
 ) => ActionCreatorMap<TCreators, TActionTypes>;
 
 export function createActionCreatorFactory<TCreators extends CreatorMap>(
-  creators: TCreators
+  creators: (handler: ExecutorHandler) => TCreators
 ): ActionCreatorFactory<TCreators> {
-  return <TActionTypes extends Record<keyof TCreators, string>>(
+  return <TActionTypes extends ActionProducerMap<TCreators>>(
     types: TActionTypes
   ) => {
-    return Object.keys(creators).reduce((result, key) => {
+    const resolverCreator: ResolverCreator = (type) => (payload, meta) => {
+      return action(type, payload!, meta!);
+    };
+
+    const factories = creators((executor) => (type) =>
+      Object.assign(executor(resolverCreator(type)), {
+        type,
+        toString: () => type,
+      })
+    );
+    const keys = Object.keys(factories) as (Extract<keyof TCreators, string>)[];
+    return keys.reduce((result, key) => {
       return Object.assign(result, {
-        [key]: createActionCreator(types[key], creators[key]),
+        [key]: types[key](factories[key]),
       });
     }, {}) as ActionCreatorMap<TCreators, TActionTypes>;
   };
