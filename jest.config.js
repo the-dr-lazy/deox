8f9eb9c85601686ce3a09e0a025ee292156f624c
Kotaro Sugawara <kotarella1110@gmail.com>
2019-12-05 19:30:15 +0900

    chore: add ts-jest testing config and scripts (#126)
    * chore: add ts-jest testing config and scripts
    
    * test: add create-action snapshot
    
    * chore: changed order of tsc testing and unit testing for scripts

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
-  runner: 'jest-runner-tsc',
-  displayName: 'tsc',
+  preset: 'ts-jest',
   testPathIgnorePatterns: [
     '(/__tests__/.*\\.dts|\\.dts\\.(test|spec))\\.tsx?$',
   ],
   roots: ['src'],
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts', '!**/__tests__/**'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json',
+    },
+  },
 }
