5210e82f593533bb9ba57ae85e668384e93b3b44
Kotaro Sugawara <kotarella1110@gmail.com>
2019-12-08 17:25:12 +0900

    fix: fixed castArray util return type

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,6 +3,6 @@ export const merge = <T extends {}>(...objs: T[]): T =>
 
 export function castArray<TValue>(
   value: TValue | ReadonlyArray<TValue>
-): ReadonlyArray<TValue> {
+): TValue[] {
   return [].concat(<any>value)
 }
