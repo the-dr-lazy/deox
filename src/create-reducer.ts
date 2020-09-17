76ac5331eaad0b2d8bf275ebeabe97144a1f2d07
Dmitry Shpak <jazzmanpw@mail.ru>
2020-09-18 00:31:25 +0300

    feat: default handler in createReducer
    related to #152

--- src/create-reducer.ts
+++ src/create-reducer.ts
@@ -32,6 +32,8 @@ export function createReducer<
   ): InferNextStateFromHandlerMap<THandlerMap> => {
     const handler = handlerMap[(<any>action).type]
 
-    return handler ? handler(<any>state, action) : state
+    return handler ? handler(<any>state, action) :
+      handlerMap.default ? handlerMap.default(<any>state, action) :
+        state
   }
 }
