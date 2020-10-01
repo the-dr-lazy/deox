b01042b8fdbaaad9ca42574baa211447487cc572
Mohammad Hasani <20781126+the-dr-lazy@users.noreply.github.com>
2020-10-01 23:44:17 +0330

    Revert "test: remove redundant tests"
    This reverts commit bc640429b63d61509aeb871b975a435a6427cb38.

--- src/__tests__/create-action-creator.dts.spec.ts
+++ src/__tests__/create-action-creator.dts.spec.ts
@@ -2,45 +2,186 @@ import { createActionCreator } from '../create-action-creator'
 
 // @dts-jest:group createActionCreator
 
-const a = createActionCreator('A')
+// @dts-jest:pass:snap
+createActionCreator('[Todo] truncate')
 
 // @dts-jest:pass:snap
-createActionCreator('A')
+const todoTruncate = createActionCreator('[Todo] truncate')
 
-// @dts-jest:fail
-a(0)
-
-// @dts-jest:pass:snap
-createActionCreator('B', resolve => (error: Error) => resolve(error))
+// @dts-jest:fail:snap
+todoTruncate('')
 
 // @dts-jest:pass:snap
-createActionCreator('C', resolve => (error: Error, meta?: { status: number }) =>
-  resolve(error, meta)
+todoTruncate()
+
+// @dts-jest:pass:snap
+createActionCreator('[Todo] fetch rejected', resolve => (error: Error) =>
+  resolve(error)
 )
 
 // @dts-jest:pass:snap
+const todoFetchRejected = createActionCreator('[Todo] fetch rejected', resolve => (error: Error) =>
+  resolve(error)
+)
+
+// @dts-jest:fail:snap
+todoFetchRejected()
+
+// @dts-jest:pass:snap
+todoFetchRejected(new Error(''))
+
+// @dts-jest:pass:snap
 createActionCreator(
-  'D',
+  '[Todo] fetch rejected',
+  resolve => (error: Error, meta?: { status: number }) => resolve(error, meta)
+)
+
+// @dts-jest:pass:snap
+const todoFetchRejected2 = createActionCreator(
+  '[Todo] fetch rejected',
+  resolve => (error: Error, meta?: { status: number }) => resolve(error, meta)
+)
+
+// @dts-jest:fail:snap
+todoFetchRejected2()
+
+// @dts-jest:pass:snap
+todoFetchRejected2(new Error(''))
+
+// @dts-jest:pass:snap
+todoFetchRejected2(new Error(''), { status: 200 })
+
+// @dts-jest:fail:snap
+todoFetchRejected2(new Error(''), { status: '' })
+
+// @dts-jest:pass:snap
+createActionCreator(
+  '[Todo] add',
   // tslint:disable-next-line:no-inferrable-types
   resolve => (name: string, completed: boolean = false) =>
     resolve({ name, completed })
 )
 
 // @dts-jest:pass:snap
-createActionCreator(
-  'E',
+const todoAdd = createActionCreator(
+  '[Todo] add',
   // tslint:disable-next-line:no-inferrable-types
   resolve => (name: string, completed: boolean = false) =>
-    resolve({ name, completed }, 'Meta')
+    resolve({ name, completed })
+)
+
+// @dts-jest:fail:snap
+todoAdd()
+
+// @dts-jest:fail:snap
+todoAdd(false);
+
+// @dts-jest:pass:snap
+todoAdd('buy presents');
+
+// @dts-jest:pass:snap
+todoAdd('buy presents', true);
+
+// @dts-jest:fail:snap
+todoAdd('buy presents', true, 42);
+
+// @dts-jest:pass:snap
+createActionCreator(
+  '[Todo] add',
+  // tslint:disable-next-line:no-inferrable-types
+  resolve => (name: string, completed: boolean = false) =>
+    resolve({ name, completed }, 'Meta data of all todos')
 )
 
 // @dts-jest:pass:snap
-createActionCreator('F', resolve => <TName>(name: TName) => resolve(name))
+const todoAdd2 = createActionCreator(
+  '[Todo] add',
+  // tslint:disable-next-line:no-inferrable-types
+  resolve => (name: string, completed: boolean = false) =>
+    resolve({ name, completed }, 'Meta data of all todos')
+)
+
+// @dts-jest:fail:snap
+todoAdd2()
 
 // @dts-jest:pass:snap
-createActionCreator('G', resolve => <TName, TMeta>(name: TName, meta: TMeta) =>
-  resolve(name, meta)
+todoAdd2('buy presents');
+
+// @dts-jest:pass:snap
+todoAdd2('buy presents', true);
+
+// @dts-jest:pass:snap
+createActionCreator('[Todo] generic', resolve => <Name>(name: Name) =>
+  resolve(name)
 )
 
 // @dts-jest:pass:snap
-createActionCreator('H', resolve => (...names: string[]) => resolve(names))
+const todoGeneric = createActionCreator('[Todo] generic', resolve => <Name>(name: Name) =>
+  resolve(name)
+)
+
+// @dts-jest:fail:snap
+todoGeneric()
+
+// @dts-jest:pass:snap
+todoGeneric('buy presents');
+
+// @dts-jest:pass:snap
+todoGeneric(false);
+
+// @dts-jest:fail:snap
+todoGeneric('buy presents', true);
+
+// @dts-jest:pass:snap
+createActionCreator(
+  '[Todo] generic',
+  resolve => <Name, Meta>(name: Name, meta: Meta) => resolve(name, meta)
+)
+
+// @dts-jest:pass:snap
+const todoGeneric2 = createActionCreator(
+  '[Todo] generic',
+  resolve => <Name, Meta>(name: Name, meta: Meta) => resolve(name, meta)
+)
+
+// @dts-jest:fail:snap
+todoGeneric2()
+
+// @dts-jest:fail:snap
+todoGeneric2(42);
+
+// @dts-jest:pass:snap
+todoGeneric2(42, 'The answer');
+
+// @dts-jest:pass:snap
+todoGeneric2('The answer', 42);
+
+// @dts-jest:fail:snap
+todoGeneric2('The answer', 42, 'Zaphod');
+
+// @dts-jest:pass:snap
+createActionCreator(
+  '[Todo] var-args',
+  resolve => (...names: string[]) => resolve(names)
+)
+
+// @dts-jest:pass:snap
+const todoVarArgs = createActionCreator(
+  '[Todo] var-args',
+  resolve => (...names: string[]) => resolve(names)
+)
+
+// @dts-jest:fail:snap
+todoVarArgs(false)
+
+// @dts-jest:pass:snap
+todoVarArgs()
+
+// @dts-jest:pass:snap
+todoVarArgs('Make Cookies')
+
+// @dts-jest:pass:snap
+todoVarArgs('Attend Christmas Party', 'Wish everyone a Merry Christmas')
+
+// @dts-jest:pass:snap
+todoVarArgs('Dress Up like Santa', 'Ignore numbers 1-3', 'Steal Christmas!')
